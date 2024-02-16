2e9cfd2c0afac24e034026fedb82923938b46fb4
Tom Nordal <tnordal@gmail.com>
2024-02-16 08:53:48 +0100

    add and comment out debuging

--- src/ftp.h
+++ src/ftp.h
@@ -6,11 +6,20 @@ void uploadFile(unsigned char *data, int length, const char *name) {
     DBG("Uploading via FTP");
 
     ftp.OpenConnection();
+    // DBG("Connected");
 
+    // DBG("Change to Binary");
     ftp.InitFile("Type I");
+
+    // DBG("Change Working Directory to " + String(ftp_remote_dir));
     ftp.ChangeWorkDir(ftp_remote_dir);
+    
+    // DBG("New File " + String(name));
     ftp.NewFile(name);
+    
+    // DBG("Uploading " + String(name));
     ftp.WriteData(data, length);
+    
     ftp.CloseFile();
     DBG("");
 }
